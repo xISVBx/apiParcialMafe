cc67df1fac497b7674e16e8415c09d865bf95144
ivan vasquez <ivansantiagovb@gmail.com>
2023-09-20 13:37:42 -0500

    Agregar archivos de proyecto.

---
+++ apiParcialMafe/Controllers/ArticulosController.cs
@@ -0,0 +1,30 @@
+﻿using apiParcialMafe.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using System.Data;
+using System.Data.Common;
+
+namespace apiParcialMafe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticulosController : ControllerBase
+    {
+        public IDbConnection _dbConnection;
+        public ArticulosController(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (var connection = _dbConnection)
+            {
+                var articulos = await connection.QueryAsync<Articulos>("SELECT * FROM articulos;");
+                return Ok(articulos);
+            }
+        }
+    }
+}
---
+++ apiParcialMafe/Models/Articulos.cs
@@ -0,0 +1,11 @@
+﻿namespace apiParcialMafe.Models
+{
+    public class Articulos
+    {
+        public int ID { get; set; }
+        public string ImagenUrl { get; set; }
+        public string Nombre { get; set; }
+        public string Vendedor { get; set; }
+        public float Calificacion { get; set; }
+    }
+}
